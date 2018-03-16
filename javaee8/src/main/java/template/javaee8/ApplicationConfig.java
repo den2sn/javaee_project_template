89c5953c003f518ba537df36c558b49852599a79
den2sn <den2sn@gmail.com>
2018-03-16 17:51:25 +0900

    Java EE 8用のプロジェクトテンプレート追加
    web.xmlを4.0にするとGlassFish5が動作しないため3.1のまま。

---
+++ javaee8/src/main/java/template/javaee8/ApplicationConfig.java
@@ -0,0 +1,11 @@
+package template.javaee8;
+
+import javax.ws.rs.ApplicationPath;
+
+/**
+ * JAX-RS Setting File.
+ */
+@ApplicationPath("api")
+public class ApplicationConfig {
+
+}
---
+++ javaee8/src/main/java/template/javaee8/ConfigurationBean.java
@@ -0,0 +1,13 @@
+package template.javaee8;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+/**
+ * JSF Setting File.
+ */
+@FacesConfig(version = FacesConfig.Version.JSF_2_3)
+@ApplicationScoped
+public class ConfigurationBean {
+
+}
